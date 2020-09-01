5fdc3d9783e1effacfc3849206e96fa06345e7d8
itaym <itaymer@gmail.com>
2020-09-01 03:50:01 +0300

    fix

--- src/redux/serialization.js
+++ src/redux/serialization.js
@@ -6,52 +6,56 @@ const DATA_ENUM = Object.freeze({
     INIT_VALUE: 4,
     INDEX_VALUE: 5,
 });
+const typesArray = ['RADIO_BUTTON', 'TEXT_INPUT', 'SELECT', 'CHECKBOX']
+
 function serialize (rowData) {
     const objData = {}
 
-    try {
-        const arrData = rowData.split('\n')
-        let minLine = Number.MAX_SAFE_INTEGER
-        let maxLine = Number.MIN_SAFE_INTEGER
-        let minColumn = Number.MAX_SAFE_INTEGER
-        let maxColumn = Number.MIN_SAFE_INTEGER
+    const arrData = rowData.split('\n')
+    let minLine = Number.MAX_SAFE_INTEGER
+    let maxLine = Number.MIN_SAFE_INTEGER
+    let minColumn = Number.MAX_SAFE_INTEGER
+    let maxColumn = Number.MIN_SAFE_INTEGER
 
-        arrData.forEach((dataLine) => {
+    arrData.forEach((dataLine) => {
+        try {
             const tmpArray = dataLine.split(';')
 
             let row = parseInt(tmpArray[DATA_ENUM.LINE_NO].toString().trim())
             let col = parseInt(tmpArray[DATA_ENUM.COLUMN_NO].toString().trim())
 
+            if (!(!isNaN(row) && !isNaN(col) && typesArray.indexOf(tmpArray[DATA_ENUM.ELEMENT_TYPE]) > -1)) return
             if (maxLine < row) maxLine = row
             if (minLine > row) minLine = row
             if (maxColumn < col) maxColumn = col
             if (minColumn > col) minColumn = col
 
             if (!objData[row]) objData[row] = {}
-            // todo: element types must match
+
             objData[row][col] = {
                 label: tmpArray[DATA_ENUM.ELEMENT_LABEL].toString().trim(),
                 type: tmpArray[DATA_ENUM.ELEMENT_TYPE].toString().trim(),
-                data: tmpArray[DATA_ENUM.INIT_VALUE].toString().trim(),
+                data: tmpArray[DATA_ENUM.INIT_VALUE]?.toString().trim(),
                 index: tmpArray[DATA_ENUM.INDEX_VALUE] ?
                     !isNaN(tmpArray[DATA_ENUM.INDEX_VALUE]) ?
-                    parseInt(tmpArray[DATA_ENUM.INDEX_VALUE].toString().trim()) :
-                    tmpArray[DATA_ENUM.INDEX_VALUE].toString().trim() :
+                        parseInt(tmpArray[DATA_ENUM.INDEX_VALUE].toString().trim()) :
+                        tmpArray[DATA_ENUM.INDEX_VALUE].toString().trim() :
                     undefined,
             }
-        })
-        for (let line = minLine; line <= maxLine; line++) {
-            if (!objData[line]) {
-                objData[line] = {}
-            }
-            for (let emptyCol = minColumn; emptyCol <= maxColumn; emptyCol++) {
-                if (!objData[line][emptyCol]) {
-                    objData[line][emptyCol] = {}
-                }
+        }
+        catch {}
+    })
+
+    for (let line = minLine; line <= maxLine; line++) {
+        if (!objData[line]) {
+            objData[line] = {}
+        }
+        for (let emptyCol = minColumn; emptyCol <= maxColumn; emptyCol++) {
+            if (!objData[line][emptyCol]) {
+                objData[line][emptyCol] = {}
             }
         }
     }
-    catch {}
 
     return objData
 }
